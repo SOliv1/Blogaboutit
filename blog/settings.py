71f01c60fbb447c92caf56f7de95bb529083146c
Sara Oliver <s.oliver1@icloud.com>
2020-05-21 15:59:29 +0000

    fix allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'atest-blog.heroku.com']
+ALLOWED_HOSTS = [('localhost'), '127.0.0.1', 'atest-blog.herokuapp.com']
 
 
 # Application definition
